2559b19389bd5e33e42ef1b73800f3fa1ae2e3e7
XU MINGLIN <18046506@myrp.edu.sg>
2019-11-04 15:14:06 +0800

    Exercises

--- helloworld.js
+++ helloworld.js
@@ -53,3 +53,45 @@ class BankAccount{
 
 var myAccount = new BankAccount("333388881111")
 myAccount.display();
+
+//#1 write a JavaScript arrow function to convert a 2D array to a comma separated values (CSV) string
+
+var arrayToString = (arr,delimiter) =>{
+    var letter1 = arr[0][0];
+    var letter2 = arr[0][1];
+    var letter3 = arr[1][0];
+    var letter4 = arr[1][1];
+
+    if(delimiter == null){
+        delimiter = ",";
+        return letter1+delimiter+letter2+delimiter+letter3+delimiter+letter4;
+
+    }
+    else{
+        return letter1+delimiter+letter2+delimiter+letter3+delimiter+letter4;
+    }
+}
+
+
+array1 = [['a','b'],['c','d']];
+var defaultOut = arrayToString(array1);
+var delimiter = arrayToString(array1,'-');
+console.log(defaultOut); //a,b,c,d
+console.log(delimiter); //a-b-c-d
+
+//#2 write a JavaScript arrow function to display the difference between wto given arrays.
+var difference = (arr1, arr2) =>{
+    var i=0;
+    while( i < arr1.length || i < arr2.length){
+        if(arr1[i] != arr2[i]){
+            return arr1[i]
+        }
+        i++;
+    }
+}
+
+array2 = ['1', '2', '3'];
+array3 = ['1', '2', '4'];
+
+var output = difference(array2, array3);
+console.log(output); //3
